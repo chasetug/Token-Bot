e9f3381081e424623986ef85fedc6a9f58f80175
Chase Tuggle <chasetug@gmail.com>
2022-04-07 17:43:12 -0500

    change to admin role

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   guild: "902117945101123584",
   ticketChannel: "902182432030203944",
   ticketRole: "929657786357387276",
-  modRole:  "902139845340708886"
+  modRole:  "902137119257030686"
 }
 
